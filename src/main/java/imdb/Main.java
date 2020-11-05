3e68fd0f4a18fbda30275147e0e4375a88eac8dd
Darina Martinčeková <DMartinc@sk.ibm.com>
2020-11-05 20:05:49 +0100

    updates

--- src/main/java/imdb/Main.java
+++ src/main/java/imdb/Main.java
@@ -4,11 +4,13 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.SelenideWait;
 import org.openqa.selenium.By;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -19,13 +21,16 @@ public class Main {
     private static String quote;
     private static String director;
     private static String review;
+    private static String VALUE;
 
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Welcome friend!");
+        System.out.println("Which movie or show do you want to explore? Please enter the name: ");
+        System.out.println();
+        VALUE = scanner.nextLine();
         open("https://www.imdb.com/");
-        System.out.println("Which movie or show do you want to explore? Please enter the name in the terminal");
-        final String VALUE = scanner.nextLine();
         $x(locators.SEARCH_INPUT).val(VALUE);
         $x(locators.SEARCH_RESULT).click();
         getSummary();
@@ -40,9 +45,9 @@ public class Main {
 
     public static void getSummary() {
         if ($x(locators.SUMMARY_TEXT).exists()) {
-            summary = $x(locators.SUMMARY_TEXT).shouldBe(visible).getText();
+            summary = $x(locators.SUMMARY_TEXT).getText();
         } else {
-            System.out.println("Seems the summary is currently not available, sorry!");
+            summary = "Seems the summary is currently not available, sorry!";
         }
     }
 
@@ -54,6 +59,7 @@ public class Main {
         System.out.println("What would you like to know? Want to see some random quote? Type 1");
         System.out.println("Want to know who produced it? Type 2");
         System.out.println("Want to see a random review? Type 3!");
+        System.out.println("Want to exit? Type 4!");
         int inp = scanner.nextInt();
         switch (inp) {
             case 1:
@@ -65,8 +71,29 @@ public class Main {
             case 3:
                 getReview();
                 break;
+            case 4:
+                exit();
+                break;
             default:
                 System.out.println("Invalid choice sorry");
+                decideAction();
+        }
+        System.out.println("Do you want to continue? For yes type 1 and for no type 2");
+        int inp2 = scanner.nextInt();
+        switch (inp2) {
+            case 1:
+                exit();
+                open("http:\\www.imdb.com");
+                $x(locators.SEARCH_INPUT).val(VALUE);
+                $x(locators.SEARCH_RESULT).click();
+                decideAction();
+                break;
+            case 2:
+                exit();
+                break;
+            default:
+                System.out.println("Invalid choice sorry");
+                decideAction();
         }
     }
 
@@ -76,7 +103,10 @@ public class Main {
         $$x(locators.MORE_ITEMS).shouldHave(CollectionCondition.sizeGreaterThan(5));
         $$x(locators.MORE_ITEMS).findBy(exactText("Quotes")).click();
         $x(locators.QUOTES_LIST).shouldBe(visible);
-        quote = $x(locators.QUOTE).shouldBe(visible).getText();
+        List<String> texts = $$x(locators.QUOTE).texts();
+        int max = texts.size();
+        int rand = (int) (Math.random() * max);
+        quote = texts.get(rand);
 
         System.out.println("A random quote for you: " + quote);
     }
@@ -89,8 +119,16 @@ public class Main {
     public static void getReview() {
         $x(locators.USER_REVIEWS).shouldBe(visible).click();
         $$x(locators.REVIEW_ITEMS).shouldHave(CollectionCondition.sizeGreaterThan(0));
-        review = $$x(locators.REVIEW_ITEMS).first().getText();
+        List<String> reviews = $$x(locators.REVIEW_ITEMS).texts();
+        int max = reviews.size();
+        int rand = (int) (Math.random() * max);
+        review = reviews.get(rand);
         System.out.println("Here is a random review: " + review);
+    }
+
+
+    public static void exit() {
+        close();
     }
 
 
